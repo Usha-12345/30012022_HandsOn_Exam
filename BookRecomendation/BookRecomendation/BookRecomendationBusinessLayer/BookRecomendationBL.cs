a3d459ca3b6dc57b3c18233e94eb9e59d9e6c559
Usha-12345 <94389736+Usha-12345@users.noreply.github.com>
2022-01-31 14:38:28 +0530

    Update BookRecomendationBL.cs

--- BookRecomendation/BookRecomendation/BookRecomendationBusinessLayer/BookRecomendationBL.cs
+++ BookRecomendation/BookRecomendation/BookRecomendationBusinessLayer/BookRecomendationBL.cs
@@ -11,11 +11,35 @@ namespace BookRecomendationBusinessLayer
     //DO NOT MODIFY THE METHOD NAMES : Adding of parameters / changing the return types of the given methods may be required. 
     public class BookRecomendationBL
     {
+        BookRecomendationDAL dalObj;
+        public BookRecomendationBL()
+        {
+            dalObj = new BookRecomendationDAL();
+        }
+        public int ConnectToDB()
+        {
+            try
+            {
+                BookRecomendationDAL dalObj = new BookRecomendationDAL();
+                return dalObj.ConnectionToDB();
+            }
+            catch (Exception)
+            {
+                return -89;
+            }
+        }
 
 
         public void ShowReviewsForBook()
         {
-           
+            
+            {
+                BookRecomendationDAL dalObj = new BookRecomendationDAL();
+                List<BookDetailsDTO> lstOfBook = dalObj.FetchAllBook();
+                //dalObj.FetchAllDept();
+                return lstOfBook;
+            }
+
         }
 
 

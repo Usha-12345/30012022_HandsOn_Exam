cc9b3e82aa181959150454af79aa28b48b43fe16
Usha-12345 <ushaaruna5520@gmail.com>
2022-01-31 14:33:44 +0530

    updated

--- BookRecomendation/BookRecomendationDataAccessLayer/BookRecomendationDAL.cs
+++ BookRecomendation/BookRecomendationDataAccessLayer/BookRecomendationDAL.cs
@@ -13,6 +13,64 @@ namespace BookRecomendationDataAccessLayer
     //DO NOT MODIFY THE METHOD NAMES : Adding of parameters / changing the return types of the given methods may be required.
     public class BookRecomendationDAL
     {
+SqlConnection conObj;
+        SqlCommand cmdObj;
+        public AdvWorksDataAccessLayer()
+        {
+            conObj = new SqlConnection(ConfigurationManager.ConnectionStrings["AdvWorksConnectionStr"].ConnectionString);
+            //contextObj = new AdventureWorks2012Context();
+        }
+
+        public int ConnectionToDB()
+        {
+            try
+            {
+                AdvWorksDataAccessLayer dalObj = new AdvWorksDataAccessLayer();
+                return dalObj.ConnectionToDB();
+            }
+            catch (Exception )
+            {
+                return -1;
+            }
+        }
+
+        public List<DeptDetailsDTO> FetchAllDept()
+        {
+            try
+            {
+                cmdObj = new SqlCommand(@"SELECT Name,GroupName FROM HumanResources.Department", conObj);
+                conObj.Open();
+                
+                List<DeptDetailsDTO> lstDept = new List<DeptDetailsDTO>();
+                while (drDept.Read())
+                {
+                    DeptDetailsDTO deptFromReader = new DeptDetailsDTO();
+                    deptFromReader.DeptName = drDept["Name"].ToString();
+                    deptFromReader.DeptGroupName = drDept["GroupName"].ToString();
+                    lstDept.Add(deptFromReader);
+                    
+                }
+                return lstDept;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conObj.Close();
+            }
+        }
+
+        public int InsertAddnewdept(DeptDetailsDTO newdeptobj, out int newdeptid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
        
 
 

04740a8e127fc223d8498e139129c507444c1db2
Usha-12345 <ushaaruna5520@gmail.com>
2022-01-31 13:34:30 +0530

    added

--- BookRecomendation/BookRecomendationBusinessLayer/BookRecomendationBL.cs
+++ BookRecomendation/BookRecomendationBusinessLayer/BookRecomendationBL.cs
@@ -3,24 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using BookDTO;
 using BookRecomendationDataAccessLayer;
-using BookRecomendationDTO;
+
 
 namespace BookRecomendationBusinessLayer
 {
     //DO NOT MODIFY THE METHOD NAMES : Adding of parameters / changing the return types of the given methods may be required. 
     public class BookRecomendationBL
     {
+         BookRecomendationDAL dalobj;
+       
+
+        public BookRecomendationBL()
+        {
+            dalobj = new BookRecomendationDAL();
+        }
+        public int ConnectToDB()
+        {
+            try
+            {
+                BookRecomendationDAL dalObj = new BookRecomendationDAL();
+                return dalObj.ConnectionToDB();
+            }
+            catch (Exception)
+            {
+                return -89;
+            }
+        }
 
 
         public void ShowReviewsForBook()
         {
+            BookRecomendationDAL dalObj = new BookRecomendationDAL();
+            List<BookRecomendationDTO> lstOfBooks = dalObj.FetchAllBooks();
            
+            return lstOfBooks;
         }
 
 
-        public void AddReviewForBook()
+        public int AddReviewForBook(BookRecomendationDTO newauthorname,out int  newauthorID )
         {
+        throw new NotImplementedException;
           
         }
       

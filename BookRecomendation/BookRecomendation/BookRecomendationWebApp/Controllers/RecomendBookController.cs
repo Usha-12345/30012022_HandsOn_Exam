27daeaae074a242d38ee26b0ac7719bb418ff1b0
Usha-12345 <94389736+Usha-12345@users.noreply.github.com>
2022-01-31 14:48:15 +0530

    added

--- BookRecomendation/BookRecomendation/BookRecomendationWebApp/Controllers/RecomendBookController.cs
+++ BookRecomendation/BookRecomendation/BookRecomendationWebApp/Controllers/RecomendBookController.cs
@@ -19,6 +19,20 @@ namespace BookRecomendationWebApp.Controllers
         public ActionResult Index()
         {
             return View();
+        
+        }
+         public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
         }
 
 
